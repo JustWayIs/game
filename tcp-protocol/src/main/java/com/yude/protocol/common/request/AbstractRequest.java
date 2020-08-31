e45579bd33af5ba0deee25f1064c38e3661c10a9
H2 <hui.he@yudesports.com>
2020-08-31 19:44:29 +0800

    还原上个操作，否则超时请求设置的 channelUserId 和 MessageType不会被序列化。

--- tcp-protocol/src/main/java/com/yude/protocol/common/request/AbstractRequest.java
+++ tcp-protocol/src/main/java/com/yude/protocol/common/request/AbstractRequest.java
@@ -1,6 +1,5 @@
 package com.yude.protocol.common.request;
 
-import com.baidu.bjf.remoting.protobuf.annotation.Ignore;
 import com.yude.protocol.common.MessageType;
 
 /**
@@ -10,11 +9,10 @@ import com.yude.protocol.common.MessageType;
  * @Declare:
  */
 public abstract class AbstractRequest implements Request{
-    @Ignore
+
     protected Long channelUserId;
 
     //要做取消快速出牌的话，需要在业务层知道消息类型：是超时执行还是玩家手动执行，手动执行就重置 玩家连续超时计数器
-    @Ignore
     protected MessageType messageType;
 
     @Override
